08a43e986ccf8e2a31e45f7bc6ecd0aef9a65d8d
bandito122 <mastrolilli.bob@gmail.com>
2017-01-08 20:34:32 +0100

    2er commit. Il faut maintenant gérer le fichier properties

---
+++ src/threadpool/MyMonitorThread.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threadpool;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ *
+ * @author bobmastrolilli
+ */
+public class MyMonitorThread implements Runnable
+{
+    private ThreadPoolExecutor executor;
+     
+    private int seconds;
+     
+    private boolean run=true;
+ 
+    public MyMonitorThread(ThreadPoolExecutor executor, int delay)
+    {
+        this.executor = executor;
+        this.seconds=delay;
+    }
+     
+    public void shutdown(){
+        this.run=false;
+    }
+ 
+    @Override
+    public void run()
+    {
+        while(run){
+                System.out.println(
+                    String.format("[monitor] [%d/%d] Active: %d, Completed: %d, Task: %d, isShutdown: %s, isTerminated: %s",
+                        this.executor.getPoolSize(),
+                        this.executor.getCorePoolSize(),
+                        this.executor.getActiveCount(),
+                        this.executor.getCompletedTaskCount(),
+                        this.executor.getTaskCount(),
+                        this.executor.isShutdown(),
+                        this.executor.isTerminated()));
+                try {
+                    Thread.sleep(seconds*10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+        }
+             
+    }
+}
---
+++ src/threadpool/RejectedExecutionHandlerImpl.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threadpool;
+
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ *
+ * @author bobmastrolilli
+ */
+public class RejectedExecutionHandlerImpl implements RejectedExecutionHandler
+{
+
+    @Override
+    public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) 
+    {
+        System.out.println(r.toString() + " is rejected...");
+    }
+    
+}
